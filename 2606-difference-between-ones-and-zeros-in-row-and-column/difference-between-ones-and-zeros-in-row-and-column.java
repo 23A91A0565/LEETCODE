35b2f4ce23e83a1bf0f1c9d3408e6e0fbbcf649b
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-07-22 11:43:11 +0530

    Time: 11 ms (51.45%) | Memory: 85.4 MB (14.09%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -3,7 +3,17 @@ class Solution {
         int ones_row[]=new int[grid.length];
         int ones_col[]=new int[grid[0].length];
         int ans[][]=new int[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++)
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    ones_row[i]++;
+                    ones_col[j]++;
+                }
+            }
+        }
+
+        /*for(int i=0;i<grid.length;i++)
         {
             int ones=0;
             for(int j=0;j<grid[0].length;j++)
@@ -27,7 +37,8 @@ class Solution {
                 }
             }
             ones_col[j]=ones;
-        }
+        }*/
+
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
