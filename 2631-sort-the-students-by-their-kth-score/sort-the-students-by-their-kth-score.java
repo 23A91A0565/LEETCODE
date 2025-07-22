7ce8b73d7b51b483da9d288bb9d78ad77426058c
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-07-22 10:35:46 +0530

    Time: 2 ms (96.18%) | Memory: 52.2 MB (74.00%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -1,25 +1,6 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-        int ans[][]=new int[score.length][score[0].length];
-        for(int i=0;i<score.length;i++)
-        {
-            for(int j=0;j<score.length;j++)
-            {
-                if(score[i][k]>score[j][k])
-                {
-                    swap(score[i],score[j],score[0].length);
-                }
-            }
-        }
+        Arrays.sort(score,(a,b)->b[k]-a[k]);
         return score;
-    }
-    public static void swap(int a[],int b[],int n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            int temp=a[i];
-            a[i]=b[i];
-            b[i]=temp;
-        }
     }
 }
