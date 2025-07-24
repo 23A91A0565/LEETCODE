8e38c26345d5c10a2e4bdec5a42eca3f4bae3097
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-07-24 19:03:50 +0530

    Time: 42 ms (86.73%) | Memory: 44.7 MB (57.43%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -1,6 +1,18 @@
 class Solution {
     public int triangularSum(int[] nums) {
-        if(nums.length==1)
+
+        int n = nums.length;
+        while (n > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            n--; 
+        }
+        return nums[0]; 
+
+        //  [OR]
+
+       /* if(nums.length==1)
         {
             return nums[0];
         }
@@ -24,7 +36,7 @@ class Solution {
         }
         else{
             return ans;
-        }
+        }*/
 
     }
 }
