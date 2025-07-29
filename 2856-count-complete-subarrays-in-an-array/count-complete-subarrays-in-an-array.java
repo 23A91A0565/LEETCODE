972bad6c87bf63d47512a0a2cb94b67ab8a32458
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-07-29 19:49:48 +0530

    Time: 7 ms (94.41%) | Memory: 44.9 MB (81.94%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,5 +1,38 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        int k = set.size();
+
+        int left = 0, right = 0, count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while (right < nums.length) {
+            int x = nums[right];
+            map.put(x, map.getOrDefault(x, 0) + 1);
+
+            while (map.size() == k) {
+                count += nums.length - right;
+
+                int y = nums[left];
+                map.put(y, map.get(y) - 1);
+                if (map.get(y) == 0) map.remove(y);
+                left++;
+            }
+
+            right++;
+        }
+
+        return count;
+    }
+}
+
+
+
+
+/*
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
         ArrayList<Integer> l=new ArrayList<>();
         int k=0;
         for(int i:nums)
@@ -21,3 +54,4 @@ class Solution {
         return res;
     }
 }
+*/
