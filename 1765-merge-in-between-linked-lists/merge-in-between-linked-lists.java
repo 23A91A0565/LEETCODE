34317facc0ed2821161006444300c02bb07b8f32
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-08-10 21:14:17 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (90.52%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode temp1 = list1;
+        while(a != 1){
+            temp1 = temp1.next;
+            a--;
+        }
+        ListNode temp2 = list1;
+        while(b != -1){
+            temp2 = temp2.next;
+            b--;
+        }
+        temp1.next = list2;
+        while(list2.next != null){
+            list2 = list2.next;
+        }
+        list2.next = temp2;
+        return list1;
+    }
+}
