75a6229637414fa8e12780e8d798305207be8b2c
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-08-17 10:23:46 +0530

    Time: 5 ms (68.28%) | Memory: 64 MB (44.05%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -8,6 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            if(fast==null) break;
+            slow=slow.next;
+        }
+        ListNode tempHead=slow.next;
+        ListNode newtempHead = reverse(tempHead);
+        int maxSum=0;
+        while(newtempHead!=null){
+            maxSum=Math.max(maxSum,newtempHead.val+head.val);
+            head=head.next;
+            newtempHead=newtempHead.next;
+        }
+        return maxSum;
+    }
+    ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
+
+
+
+
+
+
+
+
+
+ /*
 class Solution {
     public int pairSum(ListNode head) {
         ArrayList<Integer> ans=new ArrayList<>();
@@ -23,3 +65,4 @@ class Solution {
         return max;
     }
 }
+*/
