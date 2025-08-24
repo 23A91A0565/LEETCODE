6d21abc54eef5926fe0995e52a2528d27fecd6d5
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-08-24 20:44:24 +0530

    Time: 3 ms (77.92%) | Memory: 57.3 MB (11.30%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left=0; int ans=0;
+        int zeros=0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]==0) zeros++;
+            while(zeros>1) {
+                if(nums[left]==0) zeros--;
+                left++;
+            }
+            ans=Math.max(ans,i-left+1-1);           
+        }
+        return ans;
+    }
+}
