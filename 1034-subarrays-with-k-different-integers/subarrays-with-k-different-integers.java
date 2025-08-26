2a94588d3d50eabcfc6fad90795ca85c605c0e42
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-08-26 18:18:41 +0530

    Time: 49 ms (52.64%) | Memory: 48.3 MB (5.96%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,24 +1,32 @@
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-         return subarraysWithKSize(nums, k) - subarraysWithKSize(nums, k - 1);
-    }
-
-    public int subarraysWithKSize(int[] nums, int k) {
-        HashMap < Integer, Integer > hs = new HashMap < > ();
+   public int subArrays(int nums[], int k){
+        int n = nums.length;
+        HashMap<Integer , Integer> map = new HashMap<>();
+        int l =0;
+        int r = 0;
         int count = 0;
-        int i = 0;
-        int j = 0;
-        while (j < nums.length) {
-        hs.put(nums[j], hs.getOrDefault(nums[j], 0) + 1);
-        while (hs.size()>k) 
-        { 
-            hs.put(nums[i], hs.get(nums[i]) - 1);
-            if (hs.get(nums[i]) == 0)hs.remove(nums[i]);
-            i++;
+        if( k <0){
+            return 0;
         }
-        count += j - i + 1;
-        j++;
+        while( r < n){
+            map.put(nums[r] , map.getOrDefault(nums[r] , 0)+1);
+            while(map.size() >k){
+                map.put(nums[l] , map.get(nums[l]) - 1);
+                if(map.get(nums[l]) == 0){
+                    map.remove(nums[l]);
+                    
+                }
+                l++;
+            }
+            if(map.size() <= k){
+                count += r-l+1;
+            }
+            r++;
         }
         return count;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return subArrays(nums, k) - subArrays(nums , k-1);
+        
     }
 }
