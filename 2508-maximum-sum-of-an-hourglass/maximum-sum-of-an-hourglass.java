c1ade2213a96ce5a4a221b06a0ece1001a1bf36a
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-09-10 18:31:03 +0530

    Time: 8 ms (28.03%) | Memory: 44.6 MB (97.66%) - LeetSync

---
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int max=0,sum=0;
+        for(int i=1;i<grid.length-1;i++){
+            for(int j=1;j<grid[0].length-1;j++){
+                sum=grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1]+grid[i][j]+grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1];
+                max=Math.max(max,sum);
+            }
+        }
+        return max;
+    }
+}
