00e9018c65873854f9889a727a1dde5a500b739e
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-09-19 11:23:57 +0530

    Time: 735 ms (5.28%) | Memory: 56.4 MB (32.75%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -0,0 +1,36 @@
+class Spreadsheet {
+    HashMap<String, Integer> ans;
+
+    public Spreadsheet(int rows) {
+        ans = new HashMap<>();
+        for (int i = 1; i <= rows; i++) {
+            for (char j = 'A'; j <= 'Z'; j++) {
+                String cell = "" + j + i; 
+                ans.put(cell, 0);
+            }
+        }
+    }
+
+    public void setCell(String cell, int value) {
+        ans.put(cell, value);
+    }
+
+    public void resetCell(String cell) {
+        ans.put(cell, 0);
+    }
+
+    public int getValue(String formula) {
+        formula = formula.substring(1);
+        String[] parts = formula.split("\\+");
+        String s1 = parts[0];
+        String s2 = parts[1];
+
+        int left = isCellRef(s1) ? ans.getOrDefault(s1, 0) : Integer.parseInt(s1);
+        int right = isCellRef(s2) ? ans.getOrDefault(s2, 0) : Integer.parseInt(s2);
+
+        return left + right;
+    }
+    private boolean isCellRef(String s) {
+        return Character.isUpperCase(s.charAt(0));
+    }
+}
