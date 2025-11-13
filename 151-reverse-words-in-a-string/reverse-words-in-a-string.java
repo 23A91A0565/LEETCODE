597fded7b2ae4d4c26f4f1dc192b17d263bad55f
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-11-13 09:42:32 +0530

    Time: 10 ms (20.51%) | Memory: 46.9 MB (5.06%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reverseWords(String s) {
+        String st[]=s.trim().split("\\s+");
+        String res="";
+        for(int i=st.length-1;i>0;i--){
+            res+=st[i]+" ";
+        }
+        return res+st[0];
+    }
+}
