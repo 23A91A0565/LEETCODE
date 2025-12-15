94b72c8e8af35638af077bc0d6eb44a769ff3915
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-12-15 18:11:06 +0530

    Time: 2 ms (100.00%) | Memory: 84.2 MB (42.96%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -1,18 +1,31 @@
 class Solution {
     public long getDescentPeriods(int[] prices) {
-        long res=prices.length;
-        long dp[]=new long[prices.length];
-        dp[0]=0;
+        // long res=prices.length;
+        // long dp[]=new long[prices.length];
+        // dp[0]=0;
+        // for(int i=1;i<prices.length;i++){
+        //     if(prices[i-1]-prices[i]==1){
+        //         dp[i]=1+dp[i-1];
+        //     }
+        //     else{
+        //         dp[i]=0;
+        //     }
+        // }
+        // for(int i=0;i<prices.length;i++){
+        //     res+=dp[i];
+        // }
+        // return res;
+
+        long res=1;
+        long store=1;
         for(int i=1;i<prices.length;i++){
             if(prices[i-1]-prices[i]==1){
-                dp[i]=1+dp[i-1];
+                store+=1;
             }
             else{
-                dp[i]=0;
+                store=1;
             }
-        }
-        for(int i=0;i<prices.length;i++){
-            res+=dp[i];
+            res+=store;
         }
         return res;
     }
