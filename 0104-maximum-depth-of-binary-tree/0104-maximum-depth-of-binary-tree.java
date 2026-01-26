ae63e5039a3774a2355f2b4c144554d7fbbd62fa
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-01-26 10:00:35 +0530

    Time: 0 ms (100%), Space: 44.4 MB (73.81%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,23 +15,29 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        Queue<TreeNode> ans=new LinkedList<>();
-        if(root==null)return 0;
-        int c=0;
-        ans.offer(root);
-        while(!ans.isEmpty()){
-            int size=ans.size();
-            for(int i=0;i<size;i++){
-                TreeNode curr=ans.poll();
-                if(curr.left!=null){
-                    ans.offer(curr.left);
-                }
-                if(curr.right!=null){
-                    ans.offer(curr.right);
-                }
-            }
-            c+=1;
-        }
-        return c;
+        // Queue<TreeNode> ans=new LinkedList<>();
+        // if(root==null)return 0;
+        // int c=0;
+        // ans.offer(root);
+        // while(!ans.isEmpty()){
+        //     int size=ans.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode curr=ans.poll();
+        //         if(curr.left!=null){
+        //             ans.offer(curr.left);
+        //         }
+        //         if(curr.right!=null){
+        //             ans.offer(curr.right);
+        //         }
+        //     }
+        //     c+=1;
+        // }
+        // return c;
+
+        if(root==null)
+            return 0;
+        int l=maxDepth(root.left);
+        int r=maxDepth(root.right);
+        return 1+Math.max(l,r);
     }
 }
