b34ac37363ba0121aa0dc1be4c392b61b8e73352
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-01-26 09:58:14 +0530

    Time: 1 ms (12.32%), Space: 44.9 MB (9.29%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> ans=new LinkedList<>();
+        if(root==null)return 0;
+        int c=0;
+        ans.offer(root);
+        while(!ans.isEmpty()){
+            int size=ans.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr=ans.poll();
+                if(curr.left!=null){
+                    ans.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    ans.offer(curr.right);
+                }
+            }
+            c+=1;
+        }
+        return c;
+    }
+}
