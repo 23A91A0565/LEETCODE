9bedf2b29f9c0326241b403cc3213627dfbea07c
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-01-27 18:55:43 +0530

    Time: 0 ms (100%), Space: 43.6 MB (60.47%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -16,25 +16,38 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
-        if(root==null)return ans;
-        List<List<Integer>> st=new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> ele=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode curr=q.poll();
-                ele.add(curr.val);
-                if(curr.left!=null)q.offer(curr.left);
-                if(curr.right!=null)q.offer(curr.right);
-            }
-            st.add(ele);
-        }
-
-        for(List<Integer> a:st){
-            ans.add(a.get(a.size()-1));
-        }
+        calculate(root,ans,0);
         return ans;
+
+        // [ OR ]
+        // if(root==null)return ans;
+        // List<List<Integer>> st=new ArrayList<>();
+        // Queue<TreeNode> q=new LinkedList<>();
+        // q.offer(root);
+        // while(!q.isEmpty()){
+        //     int size=q.size();
+        //     List<Integer> ele=new ArrayList<>();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode curr=q.poll();
+        //         ele.add(curr.val);
+        //         if(curr.left!=null)q.offer(curr.left);
+        //         if(curr.right!=null)q.offer(curr.right);
+        //     }
+        //     st.add(ele);
+        // }
+
+        // for(List<Integer> a:st){
+        //     ans.add(a.get(a.size()-1));
+        // }
+        // return ans;
+
+
+    }
+
+    public void calculate(TreeNode root,List<Integer> ans,int depth){
+        if(root==null)return;
+        if(ans.size()==depth)ans.add(root.val);
+        calculate(root.right,ans,depth+1);
+        calculate(root.left,ans,depth+1);
     }
 }
