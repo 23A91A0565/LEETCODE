9ff9864976dea8978912c74bc3e0f8bf38240339
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-02-16 19:22:55 +0530

    Time: 0 ms (100%), Space: 42.7 MB (19.19%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,11 +1,18 @@
 class Solution {
     public int reverseBits(int n) {
-        String res=Integer.toBinaryString(n);
-        while(res.length()<32){
-            res="0"+res;
-        }
+        // String res=Integer.toBinaryString(n);
+        // while(res.length()<32){
+        //     res="0"+res;
+        // }
+        // String sb=new StringBuilder(res).reverse().toString();
+        // return Integer.parseInt(sb,2);
 
-        String sb=new StringBuilder(res).reverse().toString();
-        return Integer.parseInt(sb,2);
+        int res=0;
+        for(int i=0;i<32;i++){
+            res<<=1;
+            res|=(n&1);
+            n>>=1;
+        }
+        return res;
     }
 }
