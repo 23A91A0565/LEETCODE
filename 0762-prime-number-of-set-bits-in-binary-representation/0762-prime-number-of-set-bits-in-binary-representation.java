f88e2c0b042a06e743c14ae6db4f00ac6d54b472
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-02-21 09:45:17 +0530

    Time: 84 ms (5.02%), Space: 46.2 MB (9.9%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int c=0;
+        for(int i=left;i<=right;i++)
+        {
+            String str=Integer.toString(i,2);
+            int num_ones=0;
+            for(int j=0;j<str.length();j++)
+            {
+                if(str.charAt(j)=='1')
+                {
+                    num_ones+=1;
+                }
+            }
+            if(isPrime(num_ones))
+            {
+                c+=1;
+            }
+        }
+        return c;
+    }
+    public static boolean isPrime(int n)
+    {
+        if(n<=1)return false;
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
