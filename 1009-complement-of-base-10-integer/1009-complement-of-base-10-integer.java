e8c45c56bf904d8565454e43120726177d2e98c7
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-03-11 18:14:37 +0530

    Time: 0 ms (100%), Space: 42.1 MB (82.81%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,7 +1,14 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        String s=Integer.toBinaryString(n);
-        int len=s.length();
+        // String s=Integer.toBinaryString(n);
+        // int len=s.length();
+        if(n==0)return 1;
+        int a=n;
+        int len=0;
+        while(a!=0){
+            len++;
+            a=a>>1;
+        }
         int total=(int)Math.pow(2,len)-1;
         return total-n;
     }
