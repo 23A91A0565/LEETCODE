912e2c7d28475c32cb7a0826270e86366ae1a321
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-05-30 09:41:00 +0530

    Time: 307 ms (6.53%), Space: 83.6 MB (35.69%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashSet<Integer> ans=new HashSet<>();
+        for(int ele:arr1){
+            String s=String.valueOf(ele);
+            for(int i=1;i<=s.length();i++){
+                int st=Integer.parseInt(s.substring(0,i));
+                ans.add(st);
+            }
+        }
+
+        int res=0;
+        for(int ele:arr2){
+            String s=String.valueOf(ele);
+            for(int i=1;i<=s.length();i++){
+                int st=Integer.parseInt(s.substring(0,i));
+                if(ans.contains(st)){
+                    res=Math.max(res,i);
+                }
+            }
+        }
+        return res;
+    }
+}
