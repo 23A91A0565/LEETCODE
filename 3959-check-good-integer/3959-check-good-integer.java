9bbb42a4a5700b82f4163453c84c09baa4d801ea
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-06-14 10:57:52 +0530

    Time: 1 ms (95.3%), Space: 42.4 MB (90.01%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int s1=0,s2=0;
+        while(n!=0){
+            int r=n%10;
+            s1+=r*r;
+            s2+=r;
+            n=n/10;
+        }
+        return s1-s2>=50;
+    }
+}
