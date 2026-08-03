cdfeb417e1f36830be10b2060249d350867c75ef
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-08-03 14:30:50 +0530

    Time: 0 ms (100%), Space: 42.7 MB (49.37%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -2,13 +2,18 @@ class Solution {
     public boolean predictTheWinner(int[] nums) {
         int ans=0;
         int n=nums.length;
-        ans=solve(nums,0,n-1);
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        ans=solve(nums,0,n-1,dp);
         return ans>=0;
     }
-    public int solve(int nums[],int l,int h){
+    public int solve(int nums[],int l,int h,int dp[][]){
         if(l==h)return nums[l];
-        int ans1=nums[l]-solve(nums,l+1,h);
-        int ans2=nums[h]-solve(nums,l,h-1);
-        return Math.max(ans1,ans2);
+        if(dp[l][h]!=-1)return dp[l][h];
+        int ans1=nums[l]-solve(nums,l+1,h,dp);
+        int ans2=nums[h]-solve(nums,l,h-1,dp);
+        return dp[l][h]=Math.max(ans1,ans2);
     }
 }
