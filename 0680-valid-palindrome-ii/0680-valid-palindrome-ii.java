07eff99e1b288e347e17ab92f2a43c1168072463
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-08-04 21:26:05 +0530

    Time: 4 ms (99.02%), Space: 47.8 MB (48.35%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return solve(s,l+1,r)||solve(s,l,r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    public boolean solve(String s,int l,int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
