8efdc29d85e6c99742352ef4eae77b85ab848d7b
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-09-21 10:39:47 +0530

    Time: 2 ms (96.03%), Space: 48.3 MB (60.95%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,12 +3,12 @@ class Solution {
         int n=numbers.length;
         int i=0,j=n-1;
         while(i<j){
-            // while(i>0 && numbers[i]==numbers[i-1]){
-            //     i++;
-            // }
-            // while(j<n-1 && numbers[j]==numbers[j+1]){
-            //     j--;
-            // }
+            while(i>0 && numbers[i]==numbers[i-1]){
+                i++;
+            }
+            while(j<n-1 && numbers[j]==numbers[j+1]){
+                j--;
+            }
             if(numbers[i]+numbers[j]==target){
                 return new int[]{i+1,j+1};
             }
