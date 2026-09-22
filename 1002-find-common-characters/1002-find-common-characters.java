25e366950317e84ed3082cc1bcc5290ffe31fead
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-09-22 11:12:50 +0530

    Time: 3 ms (92.13%), Space: 44.5 MB (71.49%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,23 +1,49 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        List<String> l=new ArrayList<>();
-        for(int i=0;i<26;i++){
-            char ch=(char)('a'+i);
-            int min=Integer.MAX_VALUE;
-            for(int j=0;j<words.length;j++){
-                int freq=0;
-                for(char c:words[j].toCharArray()){
-                    if(c==ch){
-                        freq++;
-                    }
-                }
-                min=Math.min(min,freq);
+        // List<String> l=new ArrayList<>();
+        // for(int i=0;i<26;i++){
+        //     char ch=(char)('a'+i);
+        //     int min=Integer.MAX_VALUE;
+        //     for(int j=0;j<words.length;j++){
+        //         int freq=0;
+        //         for(char c:words[j].toCharArray()){
+        //             if(c==ch){
+        //                 freq++;
+        //             }
+        //         }
+        //         min=Math.min(min,freq);
+        //     }
+        //     for(int k=0;k<min;k++){
+        //         String s=ch+"";
+        //         l.add(s);
+        //     }
+        // }
+        // return l;
+
+        // [ OR ]
+
+        int[] minFreq = new int[26];
+        for (int i = 0; i < 26; i++) {
+            minFreq[i] = Integer.MAX_VALUE;
+        }
+        
+        for (String word : words) {
+            int[] charCount = new int[26];
+            for (char c : word.toCharArray()) {
+                charCount[c - 'a']++;
             }
-            for(int k=0;k<min;k++){
-                String s=ch+"";
-                l.add(s);
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charCount[i]);
             }
         }
-        return l;
+        
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                result.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        
+        return result;
     }
 }
